c4cb5ab71235179423d29687b26f6e5f7aeb50f6
Christian <chroth@Christians-MacBook-Pro-5.local>
2014-11-25 17:25:25 +0100

    Playframework

---
+++ target/scala-2.11/twirl/main/views/html/index.template.scala
@@ -0,0 +1,55 @@
+
+package views.html
+
+import play.twirl.api._
+import play.twirl.api.TemplateMagic._
+
+import play.api.templates.PlayMagic._
+import models._
+import controllers._
+import java.lang._
+import java.util._
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import play.api.i18n._
+import play.core.j.PlayMagicForJava._
+import play.mvc._
+import play.data._
+import play.api.data.Field
+import play.mvc.Http.Context.Implicit._
+import views.html._
+
+/**/
+object index extends BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with play.twirl.api.Template1[String,play.twirl.api.HtmlFormat.Appendable] {
+
+  /**/
+  def apply/*1.2*/(message: String):play.twirl.api.HtmlFormat.Appendable = {
+      _display_ {
+
+Seq[Any](format.raw/*1.19*/("""
+
+"""),_display_(/*3.2*/main("Welcome to Play")/*3.25*/ {_display_(Seq[Any](format.raw/*3.27*/("""
+
+    """),_display_(/*5.6*/play20/*5.12*/.welcome(message, style = "Java")),format.raw/*5.45*/("""
+
+""")))}),format.raw/*7.2*/("""
+"""))}
+  }
+
+  def render(message:String): play.twirl.api.HtmlFormat.Appendable = apply(message)
+
+  def f:((String) => play.twirl.api.HtmlFormat.Appendable) = (message) => apply(message)
+
+  def ref: this.type = this
+
+}
+              /*
+                  -- GENERATED --
+                  DATE: Tue Nov 25 16:49:55 CET 2014
+                  SOURCE: /Users/chroth/Rezepte/app/views/index.scala.html
+                  HASH: 2525948bb0b9a19833e9b9027e706281dd6c624c
+                  MATRIX: 723->1|828->18|856->21|887->44|926->46|958->53|972->59|1025->92|1057->95
+                  LINES: 26->1|29->1|31->3|31->3|31->3|33->5|33->5|33->5|35->7
+                  -- GENERATED --
+              */
+          
